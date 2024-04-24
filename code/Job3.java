216e606f73a6fdc675bc19a7951b142ffb80e431
Melina Moraiti <melinamoraith@gmail.com>
2024-04-24 23:12:38 +0300

    Job3: Finds max TF for every word

--- code/Job3.java
+++ code/Job3.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
+import org.w3c.dom.Text;
 
 public class Job3 {
 	// MAPPER 3
@@ -31,18 +32,25 @@ public class Job3 {
         //private int totalDocs = 3;
 	    public void reduce(Text key, Iterator<MyCompositeKey> values, OutputCollector<MyCompositeKey, MyCompositeKey> output, Reporter reporter) throws IOException {
           int noOfDocsWordExists = 0;
+          double maxTf = Double.MIN_VALUE;
+          String maxDocname = "";
 	      while (values.hasNext()) {
             MyCompositeKey currentKey = values.next();
-            String docname = currentKey.getComponents().get(0);
-            String tf = currentKey.getComponents().get(1);
+            String currentDocname = currentKey.getComponents().get(0);
+            double tf = Double.parseDouble(currentKey.getComponents().get(1));
             String ones  = currentKey.getComponents().get(2);
-            String word = key.toString();
             noOfDocsWordExists += Integer.parseInt(ones);
+            if (tf > maxTf)
+            {
+                maxTf = tf;
+                maxDocname = currentDocname;
+            }
             //double tfidf = Double.parseDouble(tf) * Math.log((double) totalDocs/ noOfDocsWordExists);
-            outputVal.setComponents(Arrays.asList(String.valueOf(tf),String.valueOf(noOfDocsWordExists)));
-            outputKey.setComponents(Arrays.asList(word,docname));
-            output.collect(outputKey, outputVal); 
 	      }	 
+          String word = key.toString();
+          outputVal.setComponents(Arrays.asList(String.valueOf(maxTf),String.valueOf(noOfDocsWordExists)));
+          outputKey.setComponents(Arrays.asList(word,maxDocname));
+          output.collect(outputKey, outputVal); 
 	    }
 	 }
 }
