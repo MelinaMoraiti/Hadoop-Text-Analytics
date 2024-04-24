6e57a89cc5be23e952cb9c499c97acedfbd13d79
Melina Moraiti <melinamoraith@gmail.com>
2024-04-24 20:48:24 +0300

    Replace with new composite key that has multiple components

--- code/Job3.java
+++ code/Job3.java
@@ -8,31 +8,41 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
 public class Job3 {
-	// MAPPER 1
+	// MAPPER 3
 	public static class CalculateTFIDFMapper extends MapReduceBase implements Mapper<MyCompositeKey, MyCompositeKey, Text, MyCompositeKey> {
 	    private Text outputKey = new Text();
 	    private final static IntWritable one = new IntWritable(1);
 	    private MyCompositeKey outputVal = new MyCompositeKey();
 	    public void map(MyCompositeKey key, MyCompositeKey value, OutputCollector<Text, MyCompositeKey> output, Reporter reporter) throws IOException {
-		outputKey.set(key.getLetter());
-		outputVal.set(key.getFilename(),one.toString());
-		output.collect(outputKey, outputVal); 
-	        
+            String docname = key.getComponents().get(1);
+            String word = key.getComponents().get(0);
+            String wordFreq = value.getComponents().get(0);
+            String noOfWords = value.getComponents().get(1);
+            double tf = (double) Integer.parseInt(wordFreq)/Integer.parseInt(noOfWords);
+            outputKey.set(word);
+		    outputVal.setComponents(Arrays.asList(docname,String.valueOf(tf),one.toString()));
+		    output.collect(outputKey, outputVal);    
 	    }
 	}
-	// REDUCER 1
-	public static class CalculateTFIDFReducer extends MapReduceBase implements Reducer<Text, MyCompositeKey, MyCompositeKey, IntWritable> {
-	    private MyCompositeKey outputKey = new MyCompositeKey();
-	    public void reduce(Text key, Iterator<MyCompositeKey> values, OutputCollector<MyCompositeKey, IntWritable> output, Reporter reporter) throws IOException {
-	      
-	      int sum = 0;
+	// REDUCER 3
+	public static class CalculateTFIDFReducer extends MapReduceBase implements Reducer<Text, MyCompositeKey, MyCompositeKey, MyCompositeKey> {
+        private MyCompositeKey outputKey = new MyCompositeKey();
+	    private MyCompositeKey outputVal = new MyCompositeKey();
+        //private int totalDocs = 3;
+	    public void reduce(Text key, Iterator<MyCompositeKey> values, OutputCollector<MyCompositeKey, MyCompositeKey> output, Reporter reporter) throws IOException {
+          int noOfDocsWordExists = 0;
 	      while (values.hasNext()) {
-		MyCompositeKey currentKey = values.next();
-		sum += Integer.parseInt(currentKey.getFilename());
-		outputKey.set(key.toString(),currentKey.getLetter());
-	      }
-	      output.collect(outputKey, new IntWritable(sum)); 
-	 
+            MyCompositeKey currentKey = values.next();
+            String docname = currentKey.getComponents().get(0);
+            String tf = currentKey.getComponents().get(1);
+            String ones  = currentKey.getComponents().get(2);
+            String word = key.toString();
+            noOfDocsWordExists += Integer.parseInt(ones);
+            //double tfidf = Double.parseDouble(tf) * Math.log((double) totalDocs/ noOfDocsWordExists);
+            outputVal.setComponents(Arrays.asList(String.valueOf(tf),String.valueOf(noOfDocsWordExists)));
+            outputKey.setComponents(Arrays.asList(word,docname));
+            output.collect(outputKey, outputVal); 
+	      }	 
 	    }
 	 }
 }
