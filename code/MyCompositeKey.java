31349626e0d88bf7f829e2c70c85b8e1bd773b76
Melina Moraiti <melinamoraith@gmail.com>
2024-04-24 18:38:12 +0300

    Feat: CompositeKey supports variable length of components

--- code/MyCompositeKey.java
+++ code/MyCompositeKey.java
@@ -3,74 +3,87 @@ package code;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hadoop.io.*;
 
 public class MyCompositeKey implements WritableComparable<MyCompositeKey> {
-    private String letter;
-    private String filename;
-    public MyCompositeKey(String letter, String filename) {
-        this.letter = letter;
-        this.filename = filename;
+    private List<String> components;
+    public MyCompositeKey(int numberOfComponents) {
+        components = new ArrayList<>(numberOfComponents);
     }
     
     public MyCompositeKey() {
+        components = new ArrayList<>();
+    }
+    public MyCompositeKey(List<String> components) {
+        this.components = components;
     }
     
-    public void set(String letter, String filename) {
-        this.letter = letter;
-        this.filename = filename;
+    public void setComponents(List<String> components) {
+        this.components = components;
     }
     
-    public String getLetter() {
-        return letter;
-    }
-    public String getFilename() {
-        return filename;
+    public List<String> getComponents() {
+        return components;
     }
     
     @Override
     public String toString() {
-        return filename + " " + letter;
+        StringBuilder builder = new StringBuilder();
+        for (String component : components) {
+            builder.append(component).append(" ");
+        }
+        return builder.toString().trim();
     }
     
     @Override
     public void readFields(DataInput in) throws IOException {
-        letter = in.readUTF();
-        filename = in.readUTF();
+        int size = in.readInt();
+        components.clear();
+        for (int = 0; i < size; i++){
+	        components.add(in.readUTF())
+        }
     }
     @Override
     public void write(DataOutput out) throws IOException {
-        out.writeUTF(letter);
-        out.writeUTF(filename);
+        out.writeInt (components.size());
+        for (String component : components) {
+	        out.writeUTF(component);
+        }
     }
     
     @Override
-    public int compareTo(MyCompositeKey t) {
-        int cmp = this.letter.compareTo(t.letter);
-        if (cmp != 0) {
-            return cmp;
+    public boolean compareTo(MyCompositeKey other) {
+        //Compare by length first
+        if (components.size() != other.components.size()) {
+	        return false;
         }
-        return this.filename.compareTo(t.filename);
+        // If sizes are equal compare by components
+	    for (int i = 0; i < components.size(); i++) { 
+	        if(!components.get(i).equals(other.components.get(i))) {
+	            return false;
+	        }
+	    }
+	    // If all components are equal return true
+        return true;
     }
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (!(obj instanceof MyCompositeKey)) {
             return false;
+        }
+        if (this == obj) {
+	        return true;
         }
         final MyCompositeKey other = (MyCompositeKey) obj;
-        if (this.letter != other.letter && (this.letter == null || !this.letter.equals(other.letter))) {
-            return false;
-        }
-        if (this.filename != other.filename && (this.filename == null || !this.filename.equals(other.filename))) {
-            return false;
-        }
-        return true;
+        return components.equals(other.components);
     }
     @Override
     public int hashCode() {
-        return this.letter.hashCode() * 163 + this.filename.hashCode();
+        return components.hashCode();
     }
 }
