a49db36ec72583e9ae6fa6e6917a10efd2caa5d2
Melina Moraiti <melinamoraith@gmail.com>
2024-04-24 22:56:33 +0300

    Update: Runs 3 jobs with 3 output directories

--- run.sh
+++ run.sh
@@ -2,17 +2,18 @@
 
 # Check for the correct number of arguments
 
-if [ "$#" -ne 6 ]; then
-	echo "Usage $0: <input dir> <intermediate dir> <output dir> <classes dir> <package_name.main_class_name> <JAR name>"
+if [ "$#" -ne 7 ]; then
+	echo "Usage $0: <input dir> <intermediate dir> <intermediate dir> <output dir> <classes dir> <package_name.main_class_name> <JAR name>"
 	exit 1
 fi
 
 INPUT_DIR="$1"
 INTERMEDIATE_DIR="$2"
-OUTPUT_DIR="$3"
-CLASSES_DIR="$4"
-MAIN="$5"
-JAR_NAME="$6"
+INTERMEDIATE_DIR2="$3"
+OUTPUT_DIR="$4"
+CLASSES_DIR="$5"
+MAIN="$6"
+JAR_NAME="$7"
 
 #Create JAR file
 if [ -e "$JAR_NAME" ]; then
@@ -24,16 +25,18 @@ jar -cvf tfidf.jar -C "$CLASSES_DIR"/ .
 
 #Execute HADOOP job
 echo "Running Hadoop job ..."
-hadoop jar tfidf.jar "$MAIN" "$INPUT_DIR" "$INTERMEDIATE_DIR" "$OUTPUT_DIR"
+hadoop jar tfidf.jar "$MAIN" "$INPUT_DIR" "$INTERMEDIATE_DIR" "$INTERMEDIATE_DIR2" "$OUTPUT_DIR"
 
 #Print results
 echo "Print result outputs from Hadoop's HDFS..."
 hadoop fs -cat "$INTERMEDIATE_DIR"/part-00000
-echo "==========================================================================="
+echo "============================================================="
+hadoop fs -cat "$INTERMEDIATE_DIR2"/part-00000
+echo "============================================================="
 hadoop fs -cat "$OUTPUT_DIR"/part-00000
 
 #Delete output directories
 echo "Removing outputs from Hadoop's HDFS ..."
-hadoop fs -rm -r "$INTERMEDIATE_DIR" "$OUTPUT_DIR"
+hadoop fs -rm -r "$INTERMEDIATE_DIR" "$INTERMEDIATE_DIR2" "$OUTPUT_DIR"
 
 echo "Job Completed successfully ..."
