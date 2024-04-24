b664917216628affbb79f0811af1436615a7d409
Melina Moraiti <melinamoraith@gmail.com>
2024-04-24 23:02:19 +0300

    fix while identation

--- code/Job1.java
+++ code/Job1.java
@@ -28,7 +28,7 @@ public class Job1 {
 	    public void reduce(MyCompositeKey key, Iterator<IntWritable> values, OutputCollector<MyCompositeKey, IntWritable> output, Reporter reporter) throws IOException {
 	      int sum = 0;
 	      while (values.hasNext()) {
-		sum += values.next().get();
+            sum += values.next().get();
 	      }
 	      output.collect(key, new IntWritable(sum));
 	    }
