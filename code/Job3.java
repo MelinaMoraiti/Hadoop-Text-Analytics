4716be13cb34c72f90b443d2864274afb03270c8
Melina Moraiti <melinamoraith@gmail.com>
2024-04-25 09:54:27 +0300

    Add TFIDF to the reducer's output values

--- code/Job3.java
+++ code/Job3.java
@@ -29,7 +29,7 @@ public class Job3 {
 	public static class CalculateTFIDFReducer extends MapReduceBase implements Reducer<Text, MyCompositeKey, MyCompositeKey, MyCompositeKey> {
         private MyCompositeKey outputKey = new MyCompositeKey();
 	    private MyCompositeKey outputVal = new MyCompositeKey();
-        //private int totalDocs = 3;
+        private int totalDocs = 3;
 	    public void reduce(Text key, Iterator<MyCompositeKey> values, OutputCollector<MyCompositeKey, MyCompositeKey> output, Reporter reporter) throws IOException {
           int noOfDocsWordExists = 0;
           double maxTf = Double.MIN_VALUE;
@@ -45,10 +45,10 @@ public class Job3 {
                 maxTf = tf;
                 maxDocname = currentDocname;
             }
-            //double tfidf = Double.parseDouble(tf) * Math.log((double) totalDocs/ noOfDocsWordExists);
 	      }	 
+          double tfidf = maxTf * Math.log((double) totalDocs/ noOfDocsWordExists);
           String word = key.toString();
-          outputVal.setComponents(Arrays.asList(String.valueOf(maxTf),String.valueOf(noOfDocsWordExists)));
+          outputVal.setComponents(Arrays.asList(String.valueOf(maxTf),String.valueOf(noOfDocsWordExists),String.valueOf(tfidf)));
           outputKey.setComponents(Arrays.asList(word,maxDocname));
           output.collect(outputKey, outputVal); 
 	    }
